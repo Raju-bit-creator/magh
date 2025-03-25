33a437d7c0491f772579db054853cf2cb44ab2d2
Raju <rajutamang3456@gmail.com>
2025-03-25 14:52:22 +0545

    state

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,35 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  //state react hook
+  const [count, setCount] = useState(0); // initialization of state
+  const [count1, setCount1] = useState(15);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+    // console.log("you click handle increment button");
+  };
+  // assignment change name using state
+  const handleDecrement = () => {
+    setCount1(count1 - 1);
+    if (count1 < 1) {
+      setCount1(0);
+    }
+  };
 
   return (
     <>
-      <div></div>
-      <h1>magh + group</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <button onClick={handleIncrement}>Increment</button>
+        <p>total count: {count}</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>thsi is my first projects</h1>
+      <div className="card">
+        <button onClick={handleDecrement}>Decrement</button>
+        <p>total count: {count1}</p>
+      </div>
     </>
   );
 }
