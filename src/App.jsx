ef69f4ad9aa45bd08cbf820cedce1f8b27861c36
Raju <rajutamang3456@gmail.com>
2025-03-26 14:55:12 +0545

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   //state react hook
-  const [count, setCount] = useState(0); // initialization of state
+  const [count, setCount] = useState(true); // initialization of state
   const [count1, setCount1] = useState(15);
 
   const handleIncrement = () => {
@@ -18,9 +19,18 @@ function App() {
       setCount1(0);
     }
   };
+  const title = "Hamro Bazzar";
+  const submit = "submit";
+  const [name, setName] = useState("rames");
+
+  function sum() {
+    setName("sanjaya");
+    console.log(name);
+  }
 
   return (
     <>
+      <Navbar title={title} sum={sum} submit={submit} /> {/* passing props */}
       <div>
         <button onClick={handleIncrement}>Increment</button>
         <p>total count: {count}</p>
