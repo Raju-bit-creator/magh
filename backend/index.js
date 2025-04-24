8a4c3f6e551be53d61e25ccd4df72577b7d9cd7c
Raju <rajutamang3456@gmail.com>
2025-04-24 14:53:16 +0545

    db connect

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const dbConnect = require("./db");
 const { chats } = require("./data/data");
 dotenv.config();
 const app = express();
+dbConnect();
 
 app.use(express.json());
 const port = process.env.PORT;
@@ -21,6 +23,8 @@ app.get("/chats/:id", (req, res) => {
   console.log(singleChat);
   res.send(singleChat);
 });
+
+app.use("/api/auth", require("./routes/Auth"));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
