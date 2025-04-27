d25b09d271095449264b86de89172b7857430277
Raju <rajutamang3456@gmail.com>
2025-04-27 14:50:50 +0545

    login

--- backend/routes/Auth.js
+++ backend/routes/Auth.js
@@ -6,7 +6,6 @@ const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 const secret = process.env.JWT_SECRET;
-console.log(secret);
 
 router.post(
   "/createuser",
@@ -46,6 +45,44 @@ router.post(
       console.log("auth", authToken);
     } catch (error) {
       res.status(500).send("internal server error");
+    }
+  }
+);
+
+//login
+router.post(
+  "/login",
+  [
+    body("email").isEmail().withMessage("enter valid email"),
+    body("password")
+      .isLength({ min: 5 })
+      .withMessage("password must be atleast 5 character"),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body; //destructuring
+
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).send({ message: "you are not register user " });
+      }
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res.status(400).send({ message: "invalid credential " });
+      }
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, secret);
+      res.json({ user, authToken });
+    } catch (error) {
+      res.status(500).send("internal server error111");
     }
   }
 );
