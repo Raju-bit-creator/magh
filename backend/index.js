1eb18c6b2220a55d2bb968cc826154d5f7ec15cd
Raju <rajutamang3456@gmail.com>
2025-05-04 14:52:25 +0545

    multer

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,9 @@
 const express = require("express");
+var cors = require("cors");
 const dotenv = require("dotenv");
+const path = require("path");
+const fs = require("fs");
+const multer = require("multer");
 const dbConnect = require("./db");
 const { chats } = require("./data/data");
 dotenv.config();
@@ -8,24 +12,46 @@ dbConnect();
 
 app.use(express.json());
 const port = process.env.PORT;
-
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-app.get("/blog", (req, res) => {
-  res.send("welcome to my new blogs");
+
+// Ensure the uploads directory exists *** multer ***
+const ensureUploadsDirectoryExists = () => {
+  const dir = path.join(__dirname, "uploads");
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
+  }
+};
+
+ensureUploadsDirectoryExists();
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    ensureUploadsDirectoryExists();
+    cb(null, path.join(__dirname, "uploads")); // Use absolute path
+  },
+  filename: function (req, file, cb) {
+    let ext = path.extname(file.originalname);
+    const uniqueSuffix =
+      Date.now() + "-" + Math.round(Math.random() * 1e9) + ext;
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
 });
-app.get("/chats", (req, res) => {
-  res.send(chats);
-});
-app.get("/chats/:id", (req, res) => {
-  let singleChat = chats.find((c) => c._id === req.params.id);
-  console.log(singleChat);
-  res.send(singleChat);
+
+const upload = multer({ storage: storage });
+
+// Serve static files from the 'uploads' directory
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+// Route to handle file uploads
+app.post("/upload", upload.single("file"), (req, res) => {
+  res.send({ filePath: `/uploads/${req.file.filename}` });
 });
 
 app.use("/api/auth", require("./routes/Auth"));
-app.use("/api/product", require("./routes/Products"));
+app.use("/api/product", upload.array("myfile"), require("./routes/Products"));
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
--- backend/routes/Products.js
+++ backend/routes/Products.js
@@ -40,16 +40,24 @@ router.post(
   async (req, res) => {
     try {
       const { title, description, price, instock } = req.body;
+      console.log("this is req boyd", req.body);
+
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
       }
+
+      let image = req.files.map((el) => {
+        return el.filename;
+      });
+      console.log(image);
 
       const product = new Product({
         title,
         description,
         price,
         instock,
+        image,
         user: req.user.id,
       });
       const saveProduct = await product.save();
@@ -83,6 +91,23 @@ router.put("/updateproduct/:id", fetchUser, async (req, res) => {
       { new: true }
     );
     res.json(product);
+  } catch (error) {
+    res.status(500).send("internal server error", error);
+  }
+});
+
+//delete product
+router.delete("/deleteproduct/:id", fetchUser, async (req, res) => {
+  try {
+    let product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).send("product not found");
+    }
+    if (!product.user || product.user.toString() !== req.user.id) {
+      return res.status(401).send("not allowed");
+    }
+    product = await Product.findByIdAndDelete(req.params.id);
+    res.json({ message: "product deleted successfully" });
   } catch (error) {
     res.status(500).send("internal server error", error);
   }
